23ea37bcddca89ef767fb2d4b490d5f68f80df86
Nazeeh Taha <87020008+nrimawi@users.noreply.github.com>
2022-04-25 02:21:22 +0300

    Create the project

---
+++ src/main/java/edu/birzeit/webservices/webservicesfirstassignment/WebServicesFirstAssignmentApplication.java
@@ -0,0 +1,13 @@
+package edu.birzeit.webservices.webservicesfirstassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServicesFirstAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebServicesFirstAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/birzeit/webservices/webservicesfirstassignment/WebServicesFirstAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.birzeit.webservices.webservicesfirstassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServicesFirstAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
